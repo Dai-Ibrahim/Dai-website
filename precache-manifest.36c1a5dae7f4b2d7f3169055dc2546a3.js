06fdc713b9b28f2ac260f1a90a8415ae50cbb435
Dai-Ibrahim <33212069+Dai-Ibrahim@users.noreply.github.com>
2021-01-20 16:22:05 -0700

    Updates

---
+++ precache-manifest.36c1a5dae7f4b2d7f3169055dc2546a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25251afe9a07ae615bec07a58ff0f9c",
+    "url": "/Dai-website/index.html"
+  },
+  {
+    "revision": "85bf348babf4a77cec58",
+    "url": "/Dai-website/static/css/main.119ef512.chunk.css"
+  },
+  {
+    "revision": "7075a57ec0d17335713b",
+    "url": "/Dai-website/static/js/2.f3fd7f90.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Dai-website/static/js/2.f3fd7f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bf348babf4a77cec58",
+    "url": "/Dai-website/static/js/main.29b70610.chunk.js"
+  },
+  {
+    "revision": "05822867879605c9d9f0",
+    "url": "/Dai-website/static/js/runtime-main.76db82a9.js"
+  }
+]);
